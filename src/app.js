889d73b0a69d96ecbbb3222ffc5986d349bde474
Wazir <ahsankamal25@gmail.com>
2023-04-12 17:09:10 +0500

    Complete with Navbar setup and all pages routes

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const path = require('path')
 require('./db/conn')
+const hbs = require('hbs')
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 // setting the path
 const staticPath = path.join(__dirname, '../public')
-console.log(path.join(__dirname, '../public'))
+const templateath = path.join(__dirname, '../templates/views')
+const partiapath = path.join(__dirname, '../templates/partials')
+
 //middlewares
 //we are telling that we are using that static path in our express
 app.use('/css', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")))
@@ -16,6 +19,8 @@ app.use('/jq', express.static(path.join(__dirname, "../node_modules/jquery/dist"
 app.use(express.static(staticPath)) 
 
 app.set("view engine", "hbs")
+app.set("views", templateath)
+hbs.registerPartials(partiapath)
 
 
 //routing
