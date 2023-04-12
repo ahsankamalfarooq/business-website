f05445496e763061af388ef0d215a0d3112076d3
Wazir <ahsankamal25@gmail.com>
2023-04-12 15:55:53 +0500

    done with jq and bootstrapm full complete setup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 const express = require('express')
+const path = require('path')
 require('./db/conn')
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+// setting the path
+const staticPath = path.join(__dirname, '../public')
+console.log(path.join(__dirname, '../public'))
+//middlewares
+//we are telling that we are using that static path in our express
+app.use('/css', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")))
+app.use('/js', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")))
+app.use('/jq', express.static(path.join(__dirname, "../node_modules/jquery/dist")))
+app.use(express.static(staticPath)) 
+
+app.set("view engine", "hbs")
+
+
 //routing
 //app.get(path, callback)
 app.get("/", (req,res) => {
-    res.send("Hello World")
+    res.render("index")
+})
+
+app.get("/contact", (req,res) => {
+    res.render("contact")
 })
 
 
